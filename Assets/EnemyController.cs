65bd069034e80a2096cd4dc727c029e2c948779f
hogehoge <hogehoge@gmail.com>
2022-02-19 14:38:30 +0900

    NavMeshAgentの練習

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+
+    public float traceDist = 10.0f;
+    NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(CheckDist());
+    }
+
+    IEnumerator CheckDist()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);//1秒間に5回処理をする
+
+            float dist = Vector3.Distance(player.position, transform.position);//敵と自分の距離
+
+            if(dist < traceDist)
+            {
+                nav.SetDestination(player.position);//目的地
+
+                nav.isStopped = false;
+            }
+            else
+            {
+                nav.isStopped = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
